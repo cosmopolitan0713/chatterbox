8d70fcc26e615dcaa708933701779c7101a2341a
cosmopolitan0713 <135200958+cosmopolitan0713@users.noreply.github.com>
2023-07-10 12:19:24 +0900

    モデル作成

---
+++ db/migrate/20230710030543_create_conversations.rb
@@ -0,0 +1,11 @@
+class CreateConversations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :conversations do |t|
+      t.text       :content,        null: false
+      t.references :user,           null: false, foreign_key: true
+      t.integer    :character_id,   null: false
+      t.integer    :background_id
+      t.timestamps
+    end
+  end
+end
