61fdda07fe5e130d3dc118a35429107a47224d45
cosmopolitan0713 <135200958+cosmopolitan0713@users.noreply.github.com>
2023-07-26 19:35:59 +0900

    修正

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,7 +1,7 @@
 console.log("previewtest")
 // charactersの定義
 const previewcharacters = [
-  { id: 1, name: 'めたん', image: 'metan1.png', profile1: '何も考えていない' },
+  { id: 1, name: 'めたん', image: 'current/public/metan1.png', profile1: '何も考えていない' },
   { id: 2, name: 'ずんだもん', image: 'zunda1.png', profile1: '何も考えていない' },
   { id: 3, name: 'つむぎ', image: 'tumugi1.png', profile1: '何も考えていない' },
   { id: 9, name: '冥鳴', image: 'meimei1.png', profile1: '何も考えていない' },
